0989f327e67b7917e09a3d77d477c2bb85bf204e
Suhyeon <70002218+onpyeong@users.noreply.github.com>
2025-01-24 15:48:51 +0900

    15654.cpp

---
+++ 01_backtracking/kang/15654.cpp
@@ -0,0 +1,45 @@
+//N과 M (5)
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int N, M;
+int arr[10];
+int answer[10];
+bool visit[10];
+
+void printArr(int k) {
+  if(k == M) {
+    for(int i = 0; i < M; i++) {
+      cout << answer[i] << " ";
+    }
+    cout << "\n";
+    return;
+  }
+  for(int i = 0; i < N; i++) {
+    if(!visit[i]) {
+      answer[k] = arr[i];
+      visit[i] = true;
+      printArr(k + 1);
+      visit[i] = false;
+    }
+  }
+}
+
+int main() 
+{
+    cin.tie(0);
+    ios::sync_with_stdio(0);
+    
+    cin >> N >> M;
+    for(int i = 0; i < N; i++) {
+      cin >> arr[i];
+    }
+    
+    sort(arr, arr + N);
+    
+    printArr(0);
+    
+    return 0;
+}
