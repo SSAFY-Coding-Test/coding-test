25f50bbb8c920865b852fe559fb946ded9331e61
Suhyeon <70002218+onpyeong@users.noreply.github.com>
2025-02-09 18:44:53 +0900

    boj: 주식_11501.cpp

---
+++ 03_greedy/kang/11501.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+using namespace std;
+
+int T, N;
+long long answer;
+int price[1000005];
+
+int main() 
+{
+    cin.tie(0);
+    ios::sync_with_stdio(0);
+    cin >> T;
+    for(int i = 0; i < T; i++) {
+      answer = 0;
+      cin >> N;
+      for(int j = 0; j < N; j++) {
+        cin >> price[j];
+      }
+      
+      int pivot, j = N;
+      bool isEnd = false;
+      while(true) {
+        if(j <= 1)
+          break;
+        pivot = price[--j];
+        while(price[j - 1] <= pivot) {
+          j--;
+          answer += pivot - price[j];
+          if(j == 0) {
+            isEnd = true;
+            break;
+          }
+        }
+        if(isEnd)
+          break;
+      }
+      
+      cout << answer << "\n";
+    }
+    
+    return 0;
+}
