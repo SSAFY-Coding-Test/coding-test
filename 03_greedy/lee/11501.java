8b7b36f29368fd3d8c9fd4f95565b01cc52adb50
dpfls0922 <dpfls0922@naver.com>
2025-02-12 23:42:22 +0900

    chore: [11501] 주석 수정

--- 03_greedy/lee/11501.java
+++ 03_greedy/lee/11501.java
@@ -8,12 +8,12 @@ public class Main {
 	private static int[] stocks;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
+
 		T = Integer.parseInt(br.readLine());
 		for (int tc = 0; tc < T; tc++) {
 			N = Integer.parseInt(br.readLine());
 			stocks = new int[N];
-			
+
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			for (int i = 0; i < N; i++) {
 				stocks[i] = Integer.parseInt(st.nextToken());
@@ -27,8 +27,8 @@ public class Main {
 	private static long findMaxProfit() {
 		long profit = 0;
 		int max = stocks[N - 1];  // 마지막 값을 최댓값으로 설정
-		
-        // 역방향으로 탐색색
+
+        // 역방향으로 탐색
 		for (int i = N - 2; i >= 0; i--) {
 			if (stocks[i] > max) {
 				max = stocks[i];
